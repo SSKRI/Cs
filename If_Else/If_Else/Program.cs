a37f5ae31457eeb0e428205972e6e79d4d4cee71
User <User@->
2022-09-19 01:22:17 +0300

    add If_Else

---
+++ If_Else/If_Else/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите первое целое число");
+int? num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе целое число");
+int? num2 = Convert.ToInt32(Console.ReadLine());
+
+if (num1 > num2)
+    Console.WriteLine($"Число {num1} больше числа {num2}");
+else if (num2 > num1)
+    Console.WriteLine($"Число {num2} больше числа {num1}");
+else
+    Console.WriteLine($"Число {num1} равно числу {num2}");
